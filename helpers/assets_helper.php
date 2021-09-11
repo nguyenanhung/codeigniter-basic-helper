248be8f5e848e6ee586d95ce1f8a13cf70bd3802
nguyenanhung <info@nguyenanhung.com>
2021-09-11 09:17:16 +0700

    Update  some assets

--- helpers/assets_helper.php
+++ helpers/assets_helper.php
@@ -19,7 +19,7 @@ if (!function_exists('assets_url')) {
      * @copyright: 713uk13m <dev@nguyenanhung.com>
      * @time     : 08/07/2021 11:56
      */
-    function assets_url($uri = '', $protocol = NULL)
+    function assets_url($uri = '', $protocol = null)
     {
         if (function_exists('base_url') && function_exists('config_item')) {
             $fileExt = substr(trim($uri), strrpos(trim($uri), '.') + 1);
@@ -48,7 +48,7 @@ if (!function_exists('templates_url')) {
      * @copyright: 713uk13m <dev@nguyenanhung.com>
      * @time     : 08/07/2021 12:12
      */
-    function templates_url($uri = '', $protocol = NULL)
+    function templates_url($uri = '', $protocol = null)
     {
         if (function_exists('base_url') && function_exists('config_item')) {
             $fileExt = substr(trim($uri), strrpos(trim($uri), '.') + 1);
@@ -62,5 +62,197 @@ if (!function_exists('templates_url')) {
         }
 
         return trim($uri);
+    }
+}
+if (!function_exists('editor_url')) {
+    /**
+     * Function editor_url
+     *
+     * @param string $uri
+     * @param null   $protocol
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 12/09/2020 47:57
+     */
+    function editor_url($uri = '', $protocol = null)
+    {
+        $uri = 'editors/' . $uri;
+
+        return assets_url($uri, $protocol);
+    }
+}
+if (!function_exists('favicon_url')) {
+    /**
+     * Function favicon_url
+     *
+     * @param string $uri
+     * @param null   $protocol
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 12/09/2020 47:51
+     */
+    function favicon_url($uri = '', $protocol = null)
+    {
+        $uri = 'favicon/' . $uri;
+
+        return assets_url($uri, $protocol);
+    }
+}
+if (!function_exists('assets_mobile')) {
+    /**
+     * Function assets_mobile
+     *
+     * @param string $uri
+     * @param null   $protocol
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 12/09/2020 47:34
+     */
+    function assets_mobile($uri = '', $protocol = null)
+    {
+        $uri = 'mobile/assets/' . $uri;
+
+        return assets_url($uri, $protocol);
+    }
+}
+if (!function_exists('assets_themes')) {
+    /**
+     * Function assets_themes
+     *
+     * @param string $themes
+     * @param string $uri
+     * @param string $asset_folder
+     * @param null   $protocol
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 12/09/2020 47:39
+     */
+    function assets_themes($themes = '', $uri = '', $asset_folder = 'yes', $protocol = null)
+    {
+        // Hooks format
+        $themes = str_replace('itravels', 'iTravels', $themes);
+        // Pattern
+        if ($themes != '') {
+            if ($asset_folder === 'no') {
+                $uri = 'themes/' . $themes . '/' . $uri;
+            } else {
+                $uri = 'themes/' . $themes . '/assets/' . $uri;
+            }
+        } else {
+            if ($asset_folder === 'no') {
+                $uri = 'themes/' . $uri;
+            } else {
+                $uri = 'themes/assets/' . $uri;
+            }
+        }
+
+        return assets_url($uri, $protocol);
+    }
+}
+if (!function_exists('assets_themes_dashboard')) {
+    /**
+     * Function assets_themes_dashboard
+     *
+     * @param string $themes
+     * @param string $uri
+     * @param string $asset_folder
+     * @param null   $protocol
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 12/09/2020 48:37
+     */
+    function assets_themes_dashboard($themes = '', $uri = '', $asset_folder = 'yes', $protocol = null)
+    {
+        if ($themes != '') {
+            if ($asset_folder === 'no') {
+                $uri = 'themes/' . $themes . '/' . $uri;
+            } else {
+                $uri = 'themes/' . $themes . '/assets/' . $uri;
+            }
+        } else {
+            if ($asset_folder === 'no') {
+                $uri = 'themes/' . $uri;
+            } else {
+                $uri = 'themes/assets/' . $uri;
+            }
+        }
+
+        return assets_url($uri, $protocol);
+    }
+}
+if (!function_exists('assets_themes_comingsoon')) {
+    /**
+     * Function assets_themes_comingsoon
+     *
+     * @param string $themes
+     * @param string $uri
+     * @param string $asset_folder
+     * @param null   $protocol
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 12/09/2020 48:22
+     */
+    function assets_themes_comingsoon($themes = '', $uri = '', $asset_folder = '', $protocol = null)
+    {
+        if ($themes != '') {
+            if ($asset_folder != '') {
+                $uri = 'themes/' . $themes . '/assets/' . $uri;
+            } else {
+                $uri = 'themes/' . $themes . '/' . $uri;
+            }
+        } else {
+            if ($asset_folder != '') {
+                $uri = 'themes/assets/' . $uri;
+            } else {
+                $uri = 'themes/' . $uri;
+            }
+        }
+
+        return assets_url($uri, $protocol);
+    }
+}
+if (!function_exists('assets_themes_error')) {
+    /**
+     * Function assets_themes_error
+     *
+     * @param string $themes
+     * @param string $uri
+     * @param string $asset_folder
+     * @param null   $protocol
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 12/09/2020 48:14
+     */
+    function assets_themes_error($themes = '', $uri = '', $asset_folder = 'yes', $protocol = null)
+    {
+        if ($themes != '') {
+            if ($asset_folder != '') {
+                $uri = 'themes/' . $themes . '/assets/' . $uri;
+            } else {
+                $uri = 'themes/' . $themes . '/' . $uri;
+            }
+        } else {
+            if ($asset_folder != '') {
+                $uri = 'themes/assets/' . $uri;
+            } else {
+                $uri = 'themes/' . $uri;
+            }
+        }
+
+        return assets_url($uri, $protocol);
     }
 }
