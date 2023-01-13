c221a99b2af62a0c4dee40090582e0fa864ffa17
nguyenanhung <info@nguyenanhung.com>
2023-01-14 01:06:32 +0700

    Release version 1.1.7.4

--- src/BaseHelper.php
+++ src/BaseHelper.php
@@ -19,8 +19,8 @@ namespace nguyenanhung\CodeIgniter\BasicHelper;
  */
 class BaseHelper
 {
-    const VERSION = '1.1.7.3';
-    const LAST_MODIFIED = '2023-01-09';
+    const VERSION = '1.1.7.4';
+    const LAST_MODIFIED = '2023-01-14';
     const AUTHOR_NAME = 'Hung Nguyen';
     const AUTHOR_EMAIL = 'dev@nguyenanhung.com';
     const PROJECT_NAME = 'CodeIgniter - Basic Helper';
