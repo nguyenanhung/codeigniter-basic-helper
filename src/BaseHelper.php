e38b52ee77da9879ac0350cd0a3e683f168b9118
nguyenanhung <info@nguyenanhung.com>
2023-03-29 11:16:36 +0700

    Update Project at Wed Mar 29 11:16:36 +07 2023

--- src/BaseHelper.php
+++ src/BaseHelper.php
@@ -19,7 +19,7 @@ namespace nguyenanhung\CodeIgniter\BasicHelper;
  */
 class BaseHelper
 {
-    const VERSION = '1.3.6';
+    const VERSION = '1.3.7';
     const LAST_MODIFIED = '2023-03-29';
     const PROJECT_NAME = 'CodeIgniter - Basic Helper';
     const AUTHOR_NAME = 'Hung Nguyen';
