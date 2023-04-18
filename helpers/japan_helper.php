a18b1a208a137351d12904eaf94c91a65db49921
nguyenanhung <info@nguyenanhung.com>
2023-04-18 10:48:48 +0700

    Add JP PrefCode

---
+++ helpers/japan_helper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Project codeigniter-basic-helper
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 18/04/2023
+ * Time: 10:44
+ */
+if (!function_exists('_japan_pref_code_')) {
+    function _japan_pref_code_()
+    {
+        return \nguyenanhung\CodeIgniter\BasicHelper\JapanUtils::getJapanPrefCode();
+    }
+}
