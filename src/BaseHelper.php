e860c61cd93c362d352f2f1955e464ce48d2419a
nguyenanhung <hungna.dev@gmail.com>
2025-01-19 21:59:23 +0700

    Refactor for PHP 8.0

--- src/BaseHelper.php
+++ src/BaseHelper.php
@@ -19,8 +19,8 @@ namespace nguyenanhung\CodeIgniter\BasicHelper;
  */
 class BaseHelper
 {
-    const VERSION = '1.6.8';
-    const LAST_MODIFIED = '2024-09-22';
+    const VERSION = '1.6.9';
+    const LAST_MODIFIED = '2025-01-19';
     const PROJECT_NAME = 'CodeIgniter - Basic Helper';
     const AUTHOR_NAME = 'Hung Nguyen';
     const AUTHOR_FULL_NAME = 'Hung Nguyen';
