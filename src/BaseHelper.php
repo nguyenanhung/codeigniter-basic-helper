09fa392db70057f4f22ce1f27aad3b0dd90d172f
nguyenanhung <hungna.dev@gmail.com>
2025-03-15 01:01:38 +0700

    English in README

--- src/BaseHelper.php
+++ src/BaseHelper.php
@@ -19,8 +19,8 @@ namespace nguyenanhung\CodeIgniter\BasicHelper;
  */
 class BaseHelper
 {
-    const VERSION = '1.6.9';
-    const LAST_MODIFIED = '2025-01-19';
+    const VERSION = '1.7.0';
+    const LAST_MODIFIED = '2025-03-15';
     const PROJECT_NAME = 'CodeIgniter - Basic Helper';
     const AUTHOR_NAME = 'Hung Nguyen';
     const AUTHOR_FULL_NAME = 'Hung Nguyen';
